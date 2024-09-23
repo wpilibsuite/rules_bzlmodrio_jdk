8a3e468667382440cab22754e7a1642e4f808bbe
PJ Reiniger <pj.reiniger@gmail.com>
2024-09-22 22:44:19 -0400

    Remove generate folder

--- generate/get_version.py
+++
@@ -1,9 +0,0 @@
-from get_toolchain_dependencies import get_toolchain_dependencies
-
-
-def main():
-    print(get_toolchain_dependencies().sanitized_version)
-
-
-if __name__ == "__main__":
-    main()
